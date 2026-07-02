89a6320e5ccee2f8ab1a32b7dbe6e6139eaf96ce
seetha-suresh13 <seetha.0706@gmail.com>
2026-07-02 11:26:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-# important content001
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
