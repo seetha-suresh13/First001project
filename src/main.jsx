0ddd51ea6832e6b0366a535c3cbaad1df9dbc8e7
seetha.0706@gmail.com <seetha.0706@gmail.com>
2026-07-02 11:34:58 +0530

    rechange

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+<<<<<<< HEAD
 # important content001
 
+=======
+>>>>>>> parent of 438e8e3 (add the content)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
